45fb6bff5ad8bf6dbc2e1f30d7be9d44cf413f67
조은 <walnutlife03@gmail.com>
2026-03-14 19:50:21 +0900

    Update 텍스트 to 수.py

---
+++ week3/텍스트 to 수.py
@@ -0,0 +1,13 @@
+print('첫 번째 수를 입력하세요:')
+a = input()
+print('두 번재 수를 입력하세요:')
+b = input()
+
+result = int(a) * int(b)
+
+print("{0}*{1}={2}".format(a,b,result))
+
+import math
+type(math.pi)
+text = "원주율은" + str(math.pi) + "입니다"
+print(text)
